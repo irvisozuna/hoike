a1cd85ce4e500ea9f4c73e0014792160edbfb8ab
Jesus Aboytia <jesus.aboytia@quantumbit.mx>
2020-04-13 16:48:29 -0700

    Validar si existe la carpeta

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -18,20 +18,9 @@ class MainController extends Controller
             'file.name'=>$require_file,
             'file.data'=>$require_file,
         ]);
-        $file = storage_path('app/layouts/').'FORMATO - NOMINA TACNA.docx';
-        if($request->has('file') && isset($request->file['data'])){
-            if($request->file['name']){
-                if (!file_exists(storage_path('app/layouts/upload'))) {
-                    mkdir(storage_path('app/layouts/upload'), 0777, true);
-                }
-                $file = storage_path('app/layouts/upload').'/'.$request->file['name'].'.docx';
-                file_put_contents($file, base64_decode($request->file['data']));
-            }else{
-                $file = storage_path('app/layouts/').'/'.time().'.docx';
-                file_put_contents($file, base64_decode($request->file['data']));
-            }
-        }
-        $document = new Document($file);
+       
+        $file_tmp = $this->saveFile($request);
+        $document = new Document($file_tmp);
 
         // Table with a spanned cell
         $document->setData($request->data);
@@ -39,6 +28,29 @@ class MainController extends Controller
         $document->generate();
         $file = $document->export();
         unlink($document->getPathFileTmp());
+
+        if($request->has('file')){
+            unlink($file_tmp);
+        }
+        
         return response()->download($file);
+    }
+    private function saveFile(Request $request){
+        $file = storage_path('app/layouts/').'FORMATO - NOMINA TACNA.docx';
+        if($request->has('file') && isset($request->file['data'])){
+            if (!file_exists(storage_path('app/layouts/upload'))) {
+                mkdir(storage_path('app/layouts/upload'), 0777, true);
+            }
+            $file = storage_path('app/layouts/upload').'/'.time().'.docx';
+            file_put_contents($file, base64_decode($request->file['data']));
+            // if($request->file['name']){
+            //     $file = storage_path('app/layouts/upload').'/'.$request->file['name'].'.docx';
+            //     file_put_contents($file, base64_decode($request->file['data']));
+            // }else{
+            //     $file = storage_path('app/layouts/').'/'.time().'.docx';
+            //     file_put_contents($file, base64_decode($request->file['data']));
+            // }
+        }
+        return $file;
     }
 }
